f2535c18f3f129064617706a4d3491f0dd968ebb
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2023-08-13 16:24:48 -0500

    V1.3.2 addition: Put movies in widget (just titles) implemented

---
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,11 @@
+
+ 
+import 'package:cinemapedia/infraestructure/datasource/moviedb_datasource.dart';
+import 'package:cinemapedia/infraestructure/repositories/movie_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final movieRepositoryProvider = Provider((ref){
+  return MovieRepositoryImpl(MoviesdbDatasource());
+});
+ 
+ 
