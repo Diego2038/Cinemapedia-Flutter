c6384abc98b6de4cbf7cd85418e9379e1d335769
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2023-09-03 19:49:12 -0500

    V1.0.5 Addition: available characters' information

---
+++ lib/presentation/providers/actors/actors_by_movie_provider.dart
@@ -0,0 +1,42 @@
+
+
+
+
+import 'package:cinemapedia/domain/entities/actor.dart';
+import 'package:cinemapedia/presentation/providers/actors/actors_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final actorsByMovieProvider = StateNotifierProvider<ActorsByMovieNotifier, Map<String,List<Actor>>>((ref) {
+  final actorsRepository = ref.watch( actorsRepositoryProvider );
+  return ActorsByMovieNotifier(getActors: actorsRepository.getActorsByMovie);
+});
+
+
+// ignore: slash_for_doc_comments
+/**
+ {
+  '5014134': <Actor>[],
+  '5014135': <Actor>[],
+  '5014136': <Actor>[],
+ }
+ */
+
+typedef GetActorsCallback = Future<List<Actor>> Function(String movieId);
+
+class ActorsByMovieNotifier extends StateNotifier<Map<String,List<Actor>>>{
+
+  final GetActorsCallback getActors;
+
+  ActorsByMovieNotifier({
+    required this.getActors
+  }): super({});
+
+  Future<void> loadActors( String movieId ) async {
+    if( state[movieId] != null ) return;
+    print("Realizando petición HTTP - actors");
+    final actors = await getActors(movieId);
+
+    state = {...state, movieId: actors };
+  }
+  
+}
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
@@ -24,9 +25,9 @@ class _MovieScreenState extends ConsumerState<MovieScreen> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     ref.read( movieInfoProvider.notifier ).loadMovie(widget.movieId);
+    ref.read( actorsByMovieProvider.notifier ).loadActors(widget.movieId);
   }
 
   @override
@@ -107,7 +108,9 @@ class _MovieDetails extends StatelessWidget {
           ),
         ),
 
-        const SizedBox(height: 100,),
+        _ActorsByMovie(movieId: movie.id.toString()),
+  
+        const SizedBox(height: 40,),
 
       ],
     );
@@ -115,6 +118,68 @@ class _MovieDetails extends StatelessWidget {
 }
 
 
+class _ActorsByMovie extends ConsumerWidget {
+
+  final String movieId;
+
+  const _ActorsByMovie({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref) {
+
+    final actorsByMovie = ref.watch( actorsByMovieProvider);
+
+    if ( actorsByMovie[movieId] == null ) return const CircularProgressIndicator(strokeWidth: 2);
+
+    final actors = actorsByMovie[movieId]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+    
+          return Container(
+            padding: const EdgeInsets.all(8.0),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                FadeInRight(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      actor.profilePath,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                
+                const SizedBox(height: 5,),
+          
+                Text(actor.name, maxLines: 2),
+          
+                Text(
+                  actor.character ?? '', 
+                  maxLines: 2,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    overflow: TextOverflow.ellipsis
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
 
 
 
@@ -137,17 +202,24 @@ class _CustomSliverAppbar extends StatelessWidget {
       foregroundColor: Colors.white,
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        title: Text(
-          movie.title,
-          style: const TextStyle(fontSize: 20),
-          textAlign: TextAlign.start,
-        ),
+        // title: Text(
+        //   movie.title,
+        //   style: const TextStyle(fontSize: 20),
+        //   textAlign: TextAlign.start,
+        // ),
         background: Stack(children: [
 
-          SizedBox.expand(child: Image.network(
-            movie.posterPath,
-            fit: BoxFit.cover,
-          ),),
+          SizedBox.expand(
+            child: 
+              Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if ( loadingProgress != null ) return const SizedBox();
+                  return FadeIn(child: child);
+                },
+              ),
+          ),
 
           const SizedBox.expand(
             child: DecoratedBox(
